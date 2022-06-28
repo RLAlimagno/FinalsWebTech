ca80d2fcf425d221d6b931ec588f107373f5b4c2
RLAlimagno <90487247+RLAlimagno@users.noreply.github.com>
2022-06-28 15:30:35 +0800

    Add files via upload

---
+++ AboutPage.php
@@ -0,0 +1,86 @@
+<?
+?>
+
+<!DOCTYPE html>
+<html lang="en"><head><meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+<title>FINALS</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+Armenian&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans&amp;display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body style="padding: 0px;background: var(--bs-dark);">
+    <nav class="navbar navbar-dark navbar-expand-md sticky-top bg-dark text-center" style="font-family: 'Noto Sans Armenian', sans-serif;padding: 9px 18px;">
+        <div class="container-fluid">
+            <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1">
+                <span class="visually-hidden">Toggle navigation</span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" data-bss-hover-animate="pulse" href="index.php" style="font-weight: bold;">Wheels.™</a>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link active" data-bss-hover-animate="pulse" href="RentRegi.php">Rent A Car</a></li>
+                    <li class="nav-item"><a class="nav-link active" data-bss-hover-animate="pulse" href="carregi.php">Car Registration</a></li>
+                    <li class="nav-item"><a class="nav-link active" data-bss-hover-animate="pulse" href="ViewRecord.php">View Records</a></li>
+                    <li class="nav-item"><a class="nav-link active" data-bss-hover-animate="pulse" href="AboutPage.php">About Us</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid" style="background: url(&quot;assets/img/bgb1.png&quot;) center / cover;padding: 0px;">
+        <div class="row" style="background: url(&quot;assets/img/bgb1.png&quot;) center / cover;padding: 61px;">
+            <div class="col-md-6" style="padding: 53px;">
+                <div>
+                    <h1 data-bss-hover-animate="bounce" style="font-family: 'Noto Sans Armenian', sans-serif;text-align: center;color: var(--bs-body-bg);font-weight: bold;background: var(--bs-dark);margin: 15px;padding: 90px;">Wheels.™</h1>
+                </div>
+            </div>
+            <div class="col-md-6" style="margin: 0px;padding: 6px;background: var(--bs-body-bg);">
+                <div class="text-center" style="color: var(--bs-body-bg);padding: 29px;margin: 39px;">
+                    <p style="margin: 5px;color: var(--bs-dark);padding: 10px;font-family: 'Open Sans', sans-serif;text-align: justify;font-size: 16px;height: 130px;">When looking for a place to rent a vehicle you came at the right website. <strong>Wheels.</strong> wants its customers to have a quality and fantastic experience when it comes to traveling.</p>
+                    <p style="text-align: justify;color: var(--bs-dark);padding: 10px;margin: 5px;font-size: 16px;height: 130px;font-family: 'Open Sans', sans-serif;">Here at <strong>Wheels.</strong>, you can find all types of cars sedan, SUV even luxury cars.&nbsp;Wheels is also available if you want to put your cars to go in rental&nbsp;<br><br></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid" style="color: var(--bs-dark);background: var(--bs-dark);">
+        <div class="row" style="margin: 10px;padding: 0px 0px 0px 0px;color: var(--bs-dark);background: var(--bs-dark);">
+            <div class="col-md-6" style="color: var(--bs-dark);background: var(--bs-dark);padding: 0px;margin: 0px;">
+                <div data-bss-hover-animate="bounce" style="background: var(--bs-body-bg);padding: 65px 65px 25px 65px;margin: 30px;border-color: var(--bs-body-bg);">
+                    <div class="text-center" style="background: var(--bs-body-bg);color: var(--bs-dark);">
+                        <img src="assets/img/parayno.jpg" style="height: 160px;">
+                    </div>
+                    <div style="margin: 10px;">
+                        <p class="text-center" style="color: var(--bs-dark);font-weight: bold;margin: 0px;">Kristian Carlo A. Parayno</p>
+                        <p style="text-align: center;">Back-End Developer</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6" style="background: var(--bs-dark);padding: 0px;margin: 0px;">
+                <div data-bss-hover-animate="bounce" style="margin: 30px;background: var(--bs-body-bg);padding: 65px 65px 25px 65px;">
+                    <div class="text-center" style="background: var(--bs-body-bg);">
+                        <img src="assets/img/polo.jpg" style="height: 160px;">
+                    </div>
+                    <div style="margin: 10px;">
+                        <p class="text-center" style="color: var(--bs-dark);background: var(--bs-body-bg);font-weight: bold;margin: 0px;">Ralph Lauren Z. Alimagno</p>
+                        <p style="text-align: center;">Front-End Developer</p>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>
+    <div class="row" style="background: var(--bs-dark);">
+        <div class="col" style="background: var(--bs-dark);">
+            <footer class="text-black-50" style="padding: 14px;margin: 13px;">
+                <p class="text-center" style="font-family: 'Noto Sans Armenian', sans-serif;color: var(--bs-secondary);">Wheels.™ © 2022</p>
+            </footer>
+        </div>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+</body>
+</html>
